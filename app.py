9f0d43d05ead39f17f048124f75b5c267e22f04d
wennyray <50888197+wennyray@users.noreply.github.com>
2020-12-15 01:34:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,6 @@ import json
 app = Flask(__name__)
 
 @app.route('/')
-def login():
-   return render_template('login.html')
-
-@app.route('/home')
 def prompt():
     return render_template('form_prompt.html')
 
@@ -16,20 +12,29 @@ def prompt():
 def result():
    if request.method == 'POST':
        
-      city = str(request.form['cityname'])
-      url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=d8c155b6234b83e3ca5d68e3e5a6deed"
-      response = requests.get(url)
-      data = response.text
-      weather_data = json.loads(data)
-
-      
       state = str(request.form['state'])
       url = "https://api.schooldigger.com/v1.2/rankings/schools/"+state+"?appID=f14c813a&appKey=560e6e0400648906909977e5c7ff3a43"
-      response = request.get(url)
+      response = requests.get(url)
       data = response.text
       school_data = json.loads(data)
+      schoolList_data = school_data["schoolList"][0:5]
+
+      schoolList = [[] for school in schoolList_data]
       
-      return render_template('form_submission.html')
-        
+      for i in range(0,5):
+          schoolList[i] += [schoolList_data[i]["schoolName"]]
+          schoolList[i] += [schoolList_data[i]["phone"]]
+          city = schoolList_data[i]["address"]["city"]
+          schoolList[i] += [city]
+
+          url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=d8c155b6234b83e3ca5d68e3e5a6deed"
+          response = requests.get(url)
+          data = response.text
+          weather_data = json.loads(data)
+
+          schoolList[i] += [weather_data["weather"][0]["main"]]
+      
+      return render_template('form_results.html', schoolList = schoolList)
+    
 if __name__ == '__main__':
     app.run(debug = True)
