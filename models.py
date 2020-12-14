516be14668b6e34d13fd3fe4b10aa9c342d018e0
wennyray <50888197+wennyray@users.noreply.github.com>
2020-12-14 22:01:26 +0800

    Update models.py

--- models.py
+++ models.py
@@ -1,3 +1,10 @@
+from flask import Flask
+from sqlalchemy import DDL, event
+from flask.ext.login import LoginManager, UserMixin
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
 app.config['SECRET_KEY'] = 'secret value'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['OAUTH_CREDENTIALS'] = {
@@ -6,3 +13,6 @@ app.config['OAUTH_CREDENTIALS'] = {
        'secret': 'eca5d89aed8b90604734d412ae76b485'
    }
 }
+
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
